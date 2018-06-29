6f0f9da92d9a1c288d10733aa1cb4eb26fe9c675
ScottBain96 <40214330>
2018-06-29 16:12:03 +0200

    added buttons
    Added layout buttons for S7 edge size screen, need to change move animations to work with speed instead of bools

--- AndroidGame/Assets/Scripts/Controller.cs
+++ AndroidGame/Assets/Scripts/Controller.cs
@@ -6,48 +6,80 @@ using UnityStandardAssets.CrossPlatformInput;
 public class Controller : MonoBehaviour {
 
 	public Rigidbody2D rb;
-	public float speedX;
 
-
-
+	//speedX can be edited easily outside of the editor in the Players Controller script.
+	public float speedX = 5;
+	bool facingRight = true;
 	Animator m_animator;
 
 
 	// Use this for initialization
 	void Start () {
+
+		//Setting up Rigidbody and Animator
+
 		rb = GetComponent<Rigidbody2D> ();
 		m_animator = GetComponent<Animator> ();
 
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	
-	
-		//TO MOVE
 
-		//rb.velocity = new Vector2 (1, rb.velocity.y);
+	void FixedUpdate () {
 
 
 		if (CrossPlatformInputManager.GetButtonDown("Jump") == true) {
-			m_animator.SetBool ("isAttack1", true);
-		
+			m_animator.SetBool ("isJump", true);
+			rb.velocity = new Vector2 (speedX, 5);
 		}
-		 else {
-			m_animator.SetBool ("isAttack1", false);
-		}
-		if (Input.GetMouseButton (0) && CrossPlatformInputManager.GetButtonDown ("Jump") == false) {
-			m_animator.SetBool ("isWalking", true);
-			rb.velocity = new Vector2 (speedX, rb.velocity.y);
+		else {
 
+			m_animator.SetBool ("isJump", false);
+			//m_animator.SetBool ("isAttack1", false);
+		}
+
+
+
+		//Movement with left/Right Buttons
+
+		if (CrossPlatformInputManager.GetButton ("Left") == true) {
+			if (transform.rotation != Quaternion.Euler (0, 180, 0))
+				transform.rotation = Quaternion.Euler (0, 180, 0);
+
+			rb.velocity = new Vector2 (-speedX, rb.velocity.y);
+		
+			m_animator.SetBool ("isWalking", true);
+		} else
+			m_animator.SetBool ("isWalking", false);
+
+		if (CrossPlatformInputManager.GetButton ("Right") == true) {
+			if (transform.rotation != Quaternion.Euler (0, 0, 0))
+				transform.rotation = Quaternion.Euler (0, 0, 0);
+
+			rb.velocity = new Vector2 (speedX, rb.velocity.y);
+	
+			m_animator.SetBool ("isWalking", true);
 		} else {
 
 			m_animator.SetBool ("isWalking", false);
 		}
 
+		//Attacking with Attack Button and setting animator boolean
+
+		if (CrossPlatformInputManager.GetButtonDown("Attack") == true) {
+			m_animator.SetBool ("isAttack1", true);
+
+		}
+		else {
+			m_animator.SetBool ("isAttack1", false);
+		}
 
 	}
+
+
+
+
+
 }
 
 
